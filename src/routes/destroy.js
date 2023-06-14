96fb9252d965921757c4afc5ac8e42eeb9d30bb1
RPE8 <vitali.shuman96@gmail.com>
2023-06-14 13:42:41 +0300

    GET Submissions with Client Side Routing

--- src/routes/destroy.js
+++ src/routes/destroy.js
@@ -3,7 +3,7 @@ import { redirect } from "react-router-dom";
 import { deleteContact } from "../contacts";
 
 export async function action({ params }) {
-  throw new Error(":)");
+  // throw new Error(":)");
   await deleteContact(params.contactId);
   return redirect(`/`);
 }
